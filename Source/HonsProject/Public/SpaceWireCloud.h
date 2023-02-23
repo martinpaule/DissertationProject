64c2a60747a98a0a392c383f3bc23957282602d4
1903300 <1903300@uad.ac.uk>
2023-02-23 17:38:52 +0000

    wireframe cloud setup
    .

--- Source/HonsProject/Public/SpaceWireCloud.h
+++ Source/HonsProject/Public/SpaceWireCloud.h
@@ -10,9 +10,32 @@
 struct CloudPoint {
 
 	FVector position;
-	
+	FVector endPosition;
+
+	FColor color;
 };
 
+struct pointHandler {
+public:
+	TArray<CloudPoint> myPoints;
+		float spawnBounds_ = 500;
+		float animSpeed_ = 100;
+
+	void updatePointValues(float dt) {
+		for (int i = 0; i < myPoints.Num(); i++) {
+
+			FVector moveV = myPoints[i].endPosition - myPoints[i].position;
+
+
+			if (moveV.Length() < 50) {
+				myPoints[i].endPosition = FMath::VRand() * FMath::FRand() * spawnBounds_;
+				return;
+			}
+			moveV.Normalize();
+			myPoints[i].position += moveV * animSpeed_ * dt;
+		}
+	}
+};
 
 
 UCLASS()
@@ -24,6 +47,8 @@ public:
 	// Sets default values for this actor's properties
 	ASpaceWireCloud();
 
+
+	
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
@@ -31,5 +56,22 @@ protected:
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
-	TArray<CloudPoint> myPoints;
+	USceneComponent* SceneComponent;
+
+
+	TArray<pointHandler> myHandlers;
+
+	void drawClouds();
+
+	UPROPERTY(Category = "CustomSetters", EditAnywhere, BlueprintReadWrite)
+		FVector velocity = FVector(10, 100, -50);
+
+	UPROPERTY(Category = "CustomSetters", EditAnywhere, BlueprintReadWrite)
+		float spawnBounds = 500;
+
+	UPROPERTY(Category = "CustomSetters", EditAnywhere, BlueprintReadWrite)
+		float animSpeed = 100;
+
+	UPROPERTY(Category = "CustomSetters", EditAnywhere, BlueprintReadWrite)
+		int pointCount = 20;
 };
